b22bd744260839c2c9715a2e9e0457022f765a8c
Abby Hilman <abbyhilman@gmail.com>
2021-07-13 16:34:49 +0700

    update filtering and sort product for module 2 session 8 topic 8 and 9

--- Module_02/Session_08/emmerce-app/src/pages/Home.jsx
+++ Module_02/Session_08/emmerce-app/src/pages/Home.jsx
@@ -6,13 +6,23 @@ import { API_URL } from "../constants/API";
 class home extends React.Component {
   state = {
     productList: [],
+    filteredProductList: [],
+    page: 1,
+    maxPage: 0,
+    itemPerPage: 6,
+    searchProductName: "",
+    searchCategory: "",
+    sortBy: "",
   };
 
   fetchProduct = () => {
     Axios.get(`${API_URL}/products`)
       .then((res) => {
-        alert("berhasil");
-        this.setState({ productList: res.data });
+        this.setState({
+          productList: res.data,
+          maxPage: Math.ceil(res.data.length / this.state.itemPerPage),
+          filteredProductList: res.data,
+        });
       })
       .catch((err) => {
         alert("Terjadi Kesalahan di server");
@@ -20,10 +30,82 @@ class home extends React.Component {
   };
 
   renderProducts() {
-    return this.state.productList.map((item) => {
+    const beginningIndex = (this.state.page - 1) * this.state.itemPerPage;
+    let rowData = [...this.state.filteredProductList];
+
+    const compareString = (a, b) => {
+      if (a.productName < b.productName) {
+        return -1;
+      }
+      if (a.productName > b.productName) {
+        return 1;
+      }
+      return 0;
+    };
+
+    switch (this.state.sortBy) {
+      case "lowPrice":
+        rowData.sort((a, b) => a.price - b.price);
+        break;
+      case "highPrice":
+        rowData.sort((a, b) => b.price - a.price);
+        break;
+      case "az":
+        rowData.sort(compareString);
+        break;
+      case "za":
+        rowData.sort((a, b) => compareString(b, a));
+        break;
+      default:
+        rowData = [...this.state.filteredProductList];
+        break;
+    }
+    const currentData = rowData.slice(
+      beginningIndex,
+      beginningIndex + this.state.itemPerPage
+    );
+    return currentData.map((item) => {
       return <ProductCard productData={item} />;
     });
   }
+
+  nextPageHandler = () => {
+    if (this.state.page < this.state.maxPage) {
+      this.setState({ page: this.state.page + 1 });
+    }
+  };
+
+  prevPageHandler = () => {
+    if (this.state.page > 1) {
+      this.setState({ page: this.state.page - 1 });
+    }
+  };
+
+  inputHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ [name]: value });
+  };
+
+  searchButtonHandler = () => {
+    const filteredProductList = this.state.productList.filter((val) => {
+      return (
+        val.productName
+          .toLowerCase()
+          .includes(this.state.searchProductName.toLowerCase()) &&
+        val.category
+          .toLowerCase()
+          .includes(this.state.searchCategory.toLowerCase())
+      );
+    });
+
+    this.setState({
+      filteredProductList,
+      maxPage: Math.ceil(filteredProductList.length / this.state.itemPerPage),
+      page: 1,
+    });
+  };
 
   componentDidMount() {
     this.fetchProduct();
@@ -41,17 +123,28 @@ class home extends React.Component {
               <div className="card-body">
                 <label htmlFor="searchProductName">Product Name</label>
                 <input
+                  onChange={this.inputHandler}
                   type="text"
                   name="searchProductName"
                   className="form-control mb-3"
                 />
                 <label htmlFor="searchCategory">Product Category</label>
-                <select name="searchCategory" className="form-control">
+                <select
+                  onChange={this.inputHandler}
+                  name="searchCategory"
+                  className="form-control"
+                >
                   <option value="">All Items</option>
-                  <option value="">Kaos</option>
-                  <option value="">Celana</option>
-                  <option value="">Aksesoris</option>
+                  <option value="kaos">Kaos</option>
+                  <option value="celana">Celana</option>
+                  <option value="aksesoris">Aksesoris</option>
                 </select>
+                <button
+                  onClick={this.searchButtonHandler}
+                  className="btn btn-primary mt-3"
+                >
+                  Search
+                </button>
               </div>
             </div>
             <div className="card mt-4">
@@ -59,21 +152,39 @@ class home extends React.Component {
                 <strong>Sort Product</strong>
               </div>
               <div className="card-body">
-                <label htmlFor="searchCategory">Sort by</label>
-                <select name="searchCategory" className="form-control">
+                <label htmlFor="sortBy">Sort by</label>
+                <select
+                  onChange={this.inputHandler}
+                  name="sortBy"
+                  className="form-control"
+                >
                   <option value="">Default</option>
-                  <option value="">Lowest Price</option>
-                  <option value="">Highest Price</option>
-                  <option value="">A-Z</option>
-                  <option value="">Z-A</option>
+                  <option value="lowPrice">Lowest Price</option>
+                  <option value="highPrice">Highest Price</option>
+                  <option value="az">A-Z</option>
+                  <option value="za">Z-A</option>
                 </select>
               </div>
             </div>
             <div className="mt-3">
               <div className="d-flex flex-row justify-content-between align-items-center">
-                <button className="btn btn-dark">{"<"}</button>
-                <div className="text-center">Page 1 of 8</div>
-                <button className="btn btn-dark">{">"}</button>
+                <button
+                  disabled={this.state.page === 1}
+                  className="btn btn-dark"
+                  onClick={this.prevPageHandler}
+                >
+                  {"<"}
+                </button>
+                <div className="text-center">
+                  Page {this.state.page} of {this.state.maxPage}
+                </div>
+                <button
+                  disabled={this.state.page === 4}
+                  className="btn btn-dark"
+                  onClick={this.nextPageHandler}
+                >
+                  {">"}
+                </button>
               </div>
             </div>
           </div>
