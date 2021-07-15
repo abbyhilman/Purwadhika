33f712058c2955b3aa5c0e8519ae5584d6e2a6ed
Abby Hilman <abbyhilman@gmail.com>
2021-07-15 14:03:34 +0700

    Update Module 2 Session 08  Topic 13

--- Module_02/Session_08/emmerce-app/src/component/ProductCard.jsx
+++ Module_02/Session_08/emmerce-app/src/component/ProductCard.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "../assets/styles/product_card.css";
+import Axios from "axios";
+import { API_URL } from "../constants/API";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getCartData } from "../redux/actions/cart";
 
 class ProductCard extends React.Component {
+  addToCartHandler = () => {
+    // Check apakah user sudah memiliki barang tsb in cart
+    Axios.get(`${API_URL}/carts`, {
+      params: {
+        userId: this.props.userGlobal.id,
+        productId: this.props.productData.id,
+      },
+    }).then((res) => {
+      if (res.data.length) {
+        // Barang sudah ada di cart user
+        Axios.patch(`${API_URL}/carts/${res.data[0].id}`, {
+          quantity: res.data[0].quantity + 1,
+        })
+          .then(() => {
+            alert("Berhasil menambahkan barang");
+            this.props.getCartData(this.props.userGlobal.id);
+          })
+          .catch(() => {
+            alert("Terjadi Kesalahan di server");
+          });
+      } else {
+        // Ketika barangnya blm ada di cart user
+        Axios.post(`${API_URL}/carts`, {
+          userId: this.props.userGlobal.id,
+          productId: this.props.productData.id,
+          price: this.props.productData.price,
+          productName: this.props.productData.productName,
+          productImage: this.props.productData.productImage,
+          quantity: 1,
+        })
+          .then(() => {
+            alert("Berhasil menambahkan barang");
+            this.props.getCartData(this.props.userGlobal.id);
+          })
+          .catch(() => {
+            alert("Terjadi Kesalahan di server");
+          });
+      }
+    });
+  };
+
   render() {
     return (
       <div className="card product-card">
@@ -21,7 +66,12 @@ class ProductCard extends React.Component {
             </span>
           </div>
           <div className="d-flex flex-row justify-content-end">
-            <button className="btn btn-primary mt-2">Add to Cart</button>
+            <button
+              onClick={this.addToCartHandler}
+              className="btn btn-primary mt-2"
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
@@ -29,4 +79,14 @@ class ProductCard extends React.Component {
   }
 }
 
-export default ProductCard;
+const mapStateToProps = (state) => {
+  return {
+    userGlobal: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  getCartData,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCard);
--- Module_02/Session_08/emmerce-app/src/pages/History.jsx
+++ Module_02/Session_08/emmerce-app/src/pages/History.jsx
@@ -1,13 +1,109 @@
 import React from "react";
+import Axios from "axios";
+import { API_URL } from "../constants/API";
+import { connect } from "react-redux";
 
 class history extends React.Component {
+  state = {
+    transactionList: [],
+    transactionDetail: [],
+  };
+
+  fetchTransactions = () => {
+    Axios.get(`${API_URL}/transactions`, {
+      params: {
+        userId: this.props.userGlobal.id,
+      },
+    })
+      .then((res) => {
+        this.setState({ transactionList: res.data });
+      })
+      .catch((err) => {
+        alert(`Terjadi Kesalahan di server`);
+      });
+  };
+
+  seeDetailsBtnHandler = (transactionDetail) => {
+    this.setState({ transactionDetail });
+  };
+
+  renderTransaction = () => {
+    return this.state.transactionList.map((item) => {
+      return (
+        <tr>
+          <td>{item.transactionDate}</td>
+          <td>{item.transactionItems.length} items(s)</td>
+          <td>Rp {item.totalPrice}</td>
+          <td>
+            <button
+              onClick={() => this.seeDetailsBtnHandler(item.transactionItems)}
+              className="btn btn-info"
+            >
+              See Details
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  renderTransactionDetailItems = () => {
+    return this.state.transactionDetail.map((item) => {
+      return (
+        <div className="d-flex my-2 flex-row justify-content-between align-items-center">
+          <span className="fw-bold">
+            {item.productName} ({item.quantity})
+          </span>
+          <span>Rp {item.price * item.quantity}</span>
+        </div>
+      );
+    });
+  };
+
+  componentDidMount() {
+    this.fetchTransactions();
+  }
+
   render() {
     return (
-      <div>
-        <h1>History Page</h1>
+      <div className="p-5">
+        <h1>Transaction History</h1>
+        <div className="row mt-5">
+          <div className="col-8">
+            <table className="table">
+              <thead className="table-light">
+                <tr>
+                  <th>Transaction Date</th>
+                  <th>Total Items</th>
+                  <th>Total Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>{this.renderTransaction()}</tbody>
+            </table>
+          </div>
+          <div className="col-4">
+            {this.state.transactionDetail.length ? (
+              <div className="card">
+                <div className="card-header">
+                  <strong>Transaction Details</strong>
+                </div>
+                <div className="card-body">
+                  {this.renderTransactionDetailItems()}
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default history;
+const mapStateToProps = (state) => {
+  return {
+    userGlobal: state.user,
+  };
+};
+
+export default connect(mapStateToProps)(history);
